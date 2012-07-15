b3c744b7399958b412de42f9b88b72b01f4f72df
Andres Ramirez <masterares@gmail.com>
2012-07-14 22:33:28 -0700

    Multiple changes:
    1) Re organized most of the assets to more adequate folders, please check that everything is alright.
    2) Added a simple system to keep track of the resources used by the guns, the system was built so that it can be inherited by another class and replace the way the GUI is draw but keeping the information access intact (more design GUI less worry on how to get the info).
    3) Checked all the resource supply system or Ammo box, it is working fine and as intended, there is a cube in scenes MK3 and MK4 that has a sample of how the system works.
    4) Added the test art scenes and added the models for the character, scene MK4 has the toon shaded version already implemented.

---
+++ Assets/Scripts/1_GunsAndRelated/7_GunResources/IResourcesHUD.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IResourcesHUD : MonoBehaviour {
+
+    protected GunInterface m_pThisGun;
+    
+    // Use this for initialization
+	void Start () {
+        m_pThisGun = gameObject.GetComponent<GunInterface>();
+	}
+	
+    void OnGUI()
+    {
+        if (!ProcessComponent())
+            return;
+
+        ProcessAllResources(OnGUIPreprocess);
+    }
+
+    void Update()
+    {
+        if (!ProcessComponent())
+            return;
+
+        ProcessAllResources(OnUpdatePreprocess);
+    }
+
+    bool ProcessComponent()
+    {
+        return m_pThisGun != null && transform.parent.renderer.enabled;
+    }
+
+    protected delegate void ProcessResource(IResource resource);
+
+    protected void ProcessAllResources(ProcessResource resourceFunct)
+    {
+        foreach (IResource resource in m_pThisGun.onShootResources)
+        {
+            resourceFunct(resource);
+        }
+        foreach (IResource resource in m_pThisGun.onActivateResources)
+        {
+            resourceFunct(resource);
+        }
+    }
+    
+    protected virtual void OnGUIPreprocess(IResource resource)
+    {
+        // simple display gui function
+        GUILayout.Label(resource.GetType().ToString() + ": " + resource.m_CurrentResourceValue + " / " + resource.m_MaxResourceValue);
+    }
+
+    protected virtual void OnUpdatePreprocess(IResource resource)
+    {
+
+    }
+
+
+
+}
