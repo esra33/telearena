5335f195034cd8042362686e3491cc6acaea266a
sebasvillegas <sebasvillegas@gmail.com>
2012-08-30 16:31:12 -0500

    prefabs

---
+++ Assets/Scripts/3_AmbientEffects/RespawnPointIdentifier.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class RespawnPointIdentifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
